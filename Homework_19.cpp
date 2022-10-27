ddbb7286942097c9d0c234d27f67d0e5f230fec6
Nastya Zbukareva <nactywa@mail.ru>
2022-10-27 14:40:20 +0300

    Добавьте файлы проекта.

---
+++ Homework_19.cpp
@@ -0,0 +1,51 @@
+﻿// Homework_19.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class Animal
+{
+public:
+    virtual void Voice()
+    {
+        cout << "!" << endl;
+    }
+};
+
+
+class Dog : public Animal
+{
+public:
+    void Voice() override
+    {
+        cout << "WOOF!" << endl;
+    }
+};
+
+
+class Cat : public Animal
+{
+public:
+    void Voice() override
+    {
+        cout << "MEOW!" << endl;
+    }
+};
+
+class Cow : public Animal
+{
+public:
+    void Voice() override
+    {
+        cout << "MOO!" << endl;
+    }
+};
+
+int main()
+{
+   
+}
+
